00fe3d978c3112ebeef6c5d18cb06e43916c8d7a
Sheng Luo <luosheng1986@gmail.com>
2013-01-23 16:34:44 +0800

    Initial Commit

---
+++ LSFontLoader/LSFontLoader.h
@@ -0,0 +1,13 @@
+//
+//  LSFontLoader.h
+//  LSFontLoader
+//
+//  Created by Luo Sheng on 13-1-23.
+//  Copyright (c) 2013年 Luo Sheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSFontLoader : NSObject
+
+@end
---
+++ LSFontLoader/LSFontLoader.m
@@ -0,0 +1,13 @@
+//
+//  LSFontLoader.m
+//  LSFontLoader
+//
+//  Created by Luo Sheng on 13-1-23.
+//  Copyright (c) 2013年 Luo Sheng. All rights reserved.
+//
+
+#import "LSFontLoader.h"
+
+@implementation LSFontLoader
+
+@end
