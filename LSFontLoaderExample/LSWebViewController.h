52fa05e8700cd2255affea0824bdd3e5425a7a07
Sheng Luo <luosheng1986@gmail.com>
2013-01-29 17:25:33 +0800

    Add LSWebViewController.

---
+++ LSFontLoaderExample/LSWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  LSWebViewController.h
+//  LSFontLoader
+//
+//  Created by Luo Sheng on 13-1-29.
+//  Copyright (c) 2013年 Luo Sheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSWebViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ LSFontLoaderExample/LSWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  LSWebViewController.m
+//  LSFontLoader
+//
+//  Created by Luo Sheng on 13-1-29.
+//  Copyright (c) 2013年 Luo Sheng. All rights reserved.
+//
+
+#import "LSWebViewController.h"
+
+@interface LSWebViewController ()
+
+@end
+
+@implementation LSWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
